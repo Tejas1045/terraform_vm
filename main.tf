510fe0cddd43aec3fc18e0d5f901c250c713b1da
Tejas1045 <82049342+Tejas1045@users.noreply.github.com>
2022-08-28 21:53:19 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "vm" {
+  ami           = "ami-02538f8925e3aa27a"
+  subnet_id     = "subnet-0ce189b1971fce56d"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "my-first-tf-node"
+  }
+}

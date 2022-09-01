7e95ccfd48a999d17df327b8f2d46344df5ecb4c
Tejas1045 <82049342+Tejas1045@users.noreply.github.com>
2022-09-01 16:58:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "vm" {
-  ami           = "ami-02538f8925e3aa27a"
-  subnet_id     = "subnet-0ce189b1971fce56d"
-  instance_type = "t3.micro"
+  ami           = "ami-05fa00d4c63e32376"
+  instance_type = "t2.micro"
   tags = {
-    Name = "my-first-tf-node"
+    Name = "my_vm"
   }
 }
 #.............................................

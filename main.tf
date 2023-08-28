53c3d46fc290ca2d3ebcd09c09308fc563bb94a8
Tejas1045 <82049342+Tejas1045@users.noreply.github.com>
2023-08-28 16:13:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  #access_key = "AKIAZ32QFUO4ONWFVUVY"
-  #secret_key = "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"
 }
 resource "aws_instance" "vm" {
   ami           = "ami-05fa00d4c63e32376"
